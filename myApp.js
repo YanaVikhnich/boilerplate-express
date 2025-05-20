01031a8e08efe166406d47b7840a5fe15ab37f7e
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-20 19:57:35 +0300

    logged all requests in the certain order

--- myApp.js
+++ myApp.js
@@ -1,13 +1,8 @@
-require('dotenv').config();
 let express = require('express');
 let app = express();
 
 
-app.get('/json', function (req, res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-});
-app.post('/json', function (req, res, next) {
+app.use('/json', function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
