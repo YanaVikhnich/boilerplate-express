e6d742c957134ce9105a3e8fa32efcd3926b8e0f
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-21 00:00:05 +0300

    Changes in line 5 and 6

--- myApp.js
+++ myApp.js
@@ -2,8 +2,9 @@ let express = require('express');
 let app = express();
 
 app.get('/:word/echo', function (req, res) {
-    res.json({ echo: word });
     console.log(req.params.word);
+    res.json({ echo: word });
+    
 });
 
 
