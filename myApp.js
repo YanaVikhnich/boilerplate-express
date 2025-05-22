ab52b7d87b542df4e60ce46e2484c26e7f70c6a5
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-22 20:01:42 +0300

    Replace word with req.params.word

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/:word/echo', function (req, res) {
-    res.json({ echo: word });
-    console.log(req.params.word);
+    res.json({ echo: req.params.word });
 });
 
 
