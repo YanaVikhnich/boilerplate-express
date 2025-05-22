8449d0a6423ba5dcaa9d8a785ef967fed3883eec
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-22 20:44:43 +0300

    Added qurystring

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let app = express();
+const querystring = require('querystring');
 
 app.route('/name').get(function (req, res) {
-    res.json({ name: 'firstname lastname' });
+    res.json({ name: req.query.name });
 }).post(function (req, res) {
-    req.query.name;
+    querystring.parse('firstname lastname');
 });
 
 
