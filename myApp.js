efe546f974b11a58f7cea6c9151a96f2ab7bd4ad
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-23 12:52:59 +0300

    Added some variation of req.query to be more specific

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ let app = express();
 
 
 app.route('/name').get( function (req, res) {
-    const { firstname, lastname } = req.query;
+    var firstname = req.query.first;
+    var lastname = req.query.last;
+    var { first: firstname, last: lastname } = req.query;
 }).post(function (req, res) {
     if (firstname && lastname) {
         res.json({ name: '${firstname} ${lastname}' });
