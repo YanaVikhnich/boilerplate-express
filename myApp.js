4ae8eb3ea236188fb2795ac13ecc418739b31e07
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-23 12:49:30 +0300

    Added POST

--- myApp.js
+++ myApp.js
@@ -2,14 +2,15 @@ let express = require('express');
 let app = express();
 
 
-app.get('/name', function (req, res) {
+app.route('/name').get( function (req, res) {
     const { firstname, lastname } = req.query;
-
+}).post(function (req, res) {
     if (firstname && lastname) {
         res.json({ name: '${firstname} ${lastname}' });
-
-    } 
+    }
 });
+    
+
 
 
 
