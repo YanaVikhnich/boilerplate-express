5ce68bac89410a21e23237586c0b700416e33f73
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-23 11:45:12 +0300

    Mama

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const querystring = require('querystring');
 app.route('/name').get(function (req, res) {
     res.json({ name: req.query.querystring });
 }).post(function (req, res) {
-    querystring = '?first=firstname&last=lastname';
+    querystring = 'firstname lastname';
 });
 
 
