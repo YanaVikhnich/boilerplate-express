c3ed9a0b4d3a033b53f1d5c3f75a85f37a230c2d
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-23 12:54:56 +0300

    Removed POST

--- myApp.js
+++ myApp.js
@@ -2,15 +2,13 @@ let express = require('express');
 let app = express();
 
 
-app.route('/name').get( function (req, res) {
+app.get('/name', function (req, res) {
     var firstname = req.query.first;
     var lastname = req.query.last;
     var { first: firstname, last: lastname } = req.query;
-}).post(function (req, res) {
     if (firstname && lastname) {
         res.json({ name: '${firstname} ${lastname}' });
-    }
-});
+    });
     
 
 
