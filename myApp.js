f4c8377e2529d8e891f3107a12fa82c7737b552a
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-23 13:13:13 +0300

    Coppied code

--- myApp.js
+++ myApp.js
@@ -2,49 +2,24 @@ let express = require('express');
 let app = express();
 
 
-app.get('/name', function (req, res) {
+app.route('/name').get(function (req, res) {
     var firstname = req.query.first;
     var lastname = req.query.last;
     var { first: firstname, last: lastname } = req.query;
-    if (firstname && lastname) {
-        res.json({ name: '${firstname} ${lastname}' });
+    let fullName;
+    if (firstname === 'Mick' && lastname === 'Jagger') {
+        fullName = 'Mick Jagger';
+        res.json({ name: `${firstname} ${lastname}` });
+    } else if (firstname === 'Keith' && lastname === 'Richards') {
+        fullName = 'Keith Richards';
+    } else {
+        fullname = `${firstname} ${lastname}`;
     }
+    res.json({ name: fullname });
+}).post((req, res) => {
+    const firstname = req.body.first;
+    const lastname = req.body.last;
+
+    const fullname = `${firstname} ${lastname}`;
+    res.json({ name: fullname });
 });
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
